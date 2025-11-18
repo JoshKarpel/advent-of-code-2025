6c85bdc77fc8e93d565f158c66af0b7517880b82
Josh Karpel <josh.karpel@gmail.com>
2025-11-18 14:40:38 -0500

    initial infra setup

---
+++ src/day_09.rs
@@ -0,0 +1,21 @@
+use crate::utils::SolverResult;
+
+fn part_1() -> usize {
+    0
+}
+
+fn part_2() -> usize {
+    0
+}
+
+pub fn solve() -> SolverResult {
+    println!("Part 1: {}", part_1());
+    println!("Part 2: {}", part_2());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
